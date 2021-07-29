b7e40881084f3ca573f8eafd3e751f6c84b93352
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-07-28 23:10:35 -0500

    Initial commit

---
+++ EasyBuilder.bat
@@ -0,0 +1,40 @@
+:Start
+@echo off
+TITLE Spigot EasyBuilder
+echo Are you sure you want to proceed? Close the window now if you dont want to proceed.
+pause
+set /p Version=Enter the version you want to build (Default latest): || set Version=latest
+set /p Name=Enter your start file name with file extension (Default start.bat): || set Name=start.bat
+set /p Minimum=Enter your minimum ram amount in megabytes (Default 1024) : || set Minimum=1024
+set /p Maximum=Enter your maximum ram amount in megabytes (Default 1024) : || set Maximum=1024
+
+:Folders
+if exist build\ (
+	goto :Building
+) else (
+	mkdir build
+	goto :Building
+)
+
+:Building
+cd build
+curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev %Version%
+ren spigot*.jar server.jar
+move server.jar ..
+cd ..
+
+:Starter
+if exist %Name% (
+	del %Name%
+	echo java -Xmx%Maximum%M -Xms%Minimum%M -jar server.jar nogui >%Name%
+	goto :Exiting
+) else (
+	echo java -Xmx%Maximum%M -Xms%Minimum%M -jar server.jar nogui >%Name%
+	goto :Exiting
+)
+
+:Exiting
+echo Hopefully it compiled right. Have a good day/night!
+pause
+exit
---
+++ README.md
@@ -0,0 +1,17 @@
+# Spigot EasyBuilder
+Simple solution to building the latest version of spigot.
+## Features
+- Simple text based UI
+- Auto-generate startup bat files for your server
+- Easy version input 
+## How to install
+1. Clone the repository
+2. Move "`EasyBuilder.bat`" file from the repository to your servers root directory
+
+And thats it!
+## How to use
+1. Open the file with command prompt
+2. Input the Minecraft version number you want to build (As long as its supported by Spigot)
+3. Input the name of your startup bat file with the file extension
+4. Input the min/max ram usage in megabytes
+5. Wait for it to compile and enjoy!
