c023f846b450dff49cda5cd8a38229addbe5cd33
AntsyBoi <38162305+AntsyBoi@users.noreply.github.com>
2021-12-14 19:10:32 -0600

    Changed and added notes to EasyBuilder.bat

--- EasyBuilder.bat
+++ EasyBuilder.bat
@@ -1,17 +1,19 @@
 :Start
 @echo off
 TITLE Spigot EasyBuilder
-echo Are you sure you want to proceed? Close the window now if you dont want to proceed.
+echo "Please make sure you have Java 16 installed before continuing."
 pause
 set /p Version=Enter the version you want to build (Default latest): || set Version=latest
 set /p Name=Enter your start file name with file extension (Default start.bat): || set Name=start.bat
-set /p Minimum=Enter your minimum ram amount in megabytes (Default 1024) : || set Minimum=1024
-set /p Maximum=Enter your maximum ram amount in megabytes (Default 1024) : || set Maximum=1024
+set /p Minimum=Enter your minimum ram amount in megabytes (Default 1024): || set Minimum=1024
+set /p Maximum=Enter your maximum ram amount in megabytes (Default 1024): || set Maximum=1024
 
 :Folders
 if exist build\ (
+	echo "Build directory already exists"
 	goto :Building
 ) else (
+	echo "Build directory doesn't exist, creating..."
 	mkdir build
 	goto :Building
 )
